bee6e61fd35a2779dc2f37471b9697766dcaae86
thanujsai <sboda@modeln.com>
2025-05-26 13:55:56 +0530

    first commit

---
+++ resources/js/Pages/Users/UserComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserComponent() {
+  return (
+    <div>
+      <h1 className="text-red-500">User Component</h1>
+    </div>
+  )
+}
+
+export default UserComponent
